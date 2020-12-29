ba5d1aca4644175724fb9a0b718dd421116a9925
= <=>
2020-12-29 12:45:57 +0100

    about me

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,13 +3,30 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  skills: any = [
+    {
+      name: 'HTML',
+      progress: 70,
+    },
+    {
+      name: 'CSS',
+      progress: 60,
+    },
+    {
+      name: 'JavaScript',
+      progress: 50,
+    },
+    {
+      name: 'TypeScript',
+      progress: 40,
+    },
+  ];
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
